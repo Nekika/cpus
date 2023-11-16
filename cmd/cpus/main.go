497e3ff5b0d0401a3cd3a50993fe6076da9118b9
Nekika <hugo.jahnke@gmail.com>
2023-11-16 08:48:28 +0100

    WS endpoint to receive usages every second

--- cmd/cpus/main.go
+++ cmd/cpus/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/shirou/gopsutil/v3/cpu"
+	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,6 +21,37 @@ func main() {
 		}
 
 		return c.JSON(http.StatusOK, times)
+	})
+
+	app.GET("/ws/cpus", func(c echo.Context) error {
+		websocket.Handler(func(conn *websocket.Conn) {
+			defer conn.Close()
+
+			var fails int
+			for {
+				if fails >= 3 {
+					log.Println("Closing connection due to too many internal errors")
+					return
+				}
+
+				usages, err := cpu.Percent(0, true)
+				if err != nil {
+					log.Println("Error getting usages:", err.Error())
+					fails += 1
+
+					continue
+				}
+
+				if err := websocket.JSON.Send(conn, usages); err != nil {
+					log.Println("Error sending message:", err.Error())
+					fails += 1
+				}
+
+				time.Sleep(time.Second * 1)
+			}
+		}).ServeHTTP(c.Response(), c.Request())
+
+		return nil
 	})
 
 	log.Fatal(app.Start(":4356"))
--- go.mod
+++ go.mod
@@ -20,8 +20,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
